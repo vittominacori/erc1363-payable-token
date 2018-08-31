c775cf41f732fe2d45e70d53c15b871877215e71
vittominacori <vittoriominacori@gmail.com>
2018-08-31 11:44:04 +0200

    use testrpc for coverage

--- .solcover.js
+++ .solcover.js
@@ -1,5 +1,4 @@
 module.exports = {
-  copyNodeModules: true,
   norpc: true,
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
