3d81e0a4bae85a55aae3be12c731d9610d34ef8d
vittominacori <vittoriominacori@gmail.com>
2018-09-02 13:49:17 +0200

    removed skip files

--- .solcover.js
+++ .solcover.js
@@ -2,7 +2,5 @@ module.exports = {
   norpc: true,
   testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
   compileCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle compile --network coverage',
-  skipFiles: [
-    'mocks'
-  ]
+  skipFiles: []
 };
