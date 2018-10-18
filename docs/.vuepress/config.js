16a6c364caaa18fd283e4ec0ff140f41dc351d55
vittominacori <vittoriominacori@gmail.com>
2018-10-18 18:27:53 +0200

    fix doc path

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'ERC-1363 Payable Token',
   description: 'The ERC-1363 is an ERC-20 compatible token that can make a callback on the receiver contract to notify token transfers or token approvals',
-  base: '/erc-payable-token/',
+  base: '/erc1363-payable-token/',
   ga: 'UA-115756440-2',
   themeConfig: {
     sidebar: 'auto',
