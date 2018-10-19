fab50892cf6514d20fa29b3f172a4320d9909414
vittominacori <vittoriominacori@gmail.com>
2018-10-19 14:01:42 +0200

    updated docs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  title: 'ERC-1363 Payable Token',
+  title: 'An ERC-20 token that can be used for payments',
   description: 'The ERC-1363 is an ERC-20 compatible token that can make a callback on the receiver contract to notify token transfers or token approvals',
   base: '/erc1363-payable-token/',
   ga: 'UA-115756440-2',
   themeConfig: {
+    repo: 'vittominacori/erc1363-payable-token',
     sidebar: 'auto',
   },
 };
