8c257f691171664aae287c9dbdd1d833f136edf4
vittominacori <vittoriominacori@gmail.com>
2020-04-26 19:00:18 +0200

    add social image

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,15 @@ module.exports = {
   description: 'The ERC-1363 is an ERC-20 compatible token that can make a callback on the receiver contract to notify token transfers or token approvals',
   base: '/erc1363-payable-token/',
   ga: 'UA-115756440-2',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: 'https://vittominacori.github.io/erc1363-payable-token' }],
+    ['meta', { property: 'og:image', content: 'https://vittominacori.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { property: 'twitter:image', content: 'https://vittominacori.github.io/erc1363-payable-token/assets/images/erc1363-payable-token.jpg' }], // eslint-disable-line max-len
+    ['meta', { property: 'twitter:title', content: 'ERC-1363 Payable Token | An ERC-20 token that can be used for payments' }],
+  ],
   themeConfig: {
     repo: 'vittominacori/erc1363-payable-token',
     sidebar: 'auto',
