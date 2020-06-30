c39ed7706a775c0661c8f6662e0c85004b2a1bd1
vittominacori <vittoriominacori@gmail.com>
2020-06-30 23:54:32 +0200

    update dependencies

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,11 @@ module.exports = {
   title: 'An ERC-20 token that can be used for payments',
   description: 'The ERC-1363 is an ERC-20 compatible token that can make a callback on the receiver contract to notify token transfers or token approvals',
   base: '/erc1363-payable-token/',
-  ga: 'UA-115756440-2',
+  plugins: [
+    ['@vuepress/google-analytics', {
+      ga: 'UA-115756440-2'
+    }]
+  ],
   head: [
     ['link', { rel: 'icon', href: '/favicon.ico' }],
     ['meta', { property: 'og:type', content: 'website' }],
