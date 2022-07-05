11c8e10cb679b60f74bbd0d0e16ae4814cc29c65
Vittorio Minacori <vittoriominacori@gmail.com>
2022-07-05 11:31:12 +0200

    feat: add flat and analysis files

---
+++ scripts/analyze.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+npx surya inheritance flat/ERC1363.flat.sol | dot -Tpng > analysis/inheritance-tree/ERC1363.png
+
+npx surya graph flat/ERC1363.flat.sol | dot -Tpng > analysis/control-flow/ERC1363.png
+
+npx surya mdreport analysis/description-table/ERC1363.md flat/ERC1363.flat.sol
+
+npx sol2uml flat/ERC1363.flat.sol -o analysis/uml/ERC1363.svg
---
+++ scripts/flat.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+npx truffle-flattener contracts/token/ERC1363/ERC1363.sol > flat/ERC1363.flat.sol
