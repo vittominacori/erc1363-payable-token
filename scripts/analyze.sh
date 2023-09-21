4a67e8d55279430a6b86eb25ac7ffd0d108718bd
Vittorio Minacori <vittoriominacori@gmail.com>
2023-09-21 17:22:12 +0200

    fix: analyze script

--- scripts/analyze.sh
+++ scripts/analyze.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-npx surya inheritance dist/ERC1363.dist.sol | dot -Tpng > analysis/inheritance-tree/ERC1363.png
+npx surya inheritance flat/ERC1363.flat.sol | dot -Tpng > analysis/inheritance-tree/ERC1363.png
 
-npx surya graph dist/ERC1363.dist.sol | dot -Tpng > analysis/control-flow/ERC1363.png
+npx surya graph flat/ERC1363.flat.sol | dot -Tpng > analysis/control-flow/ERC1363.png
 
-npx surya mdreport analysis/description-table/ERC1363.md dist/ERC1363.dist.sol
+npx surya mdreport analysis/description-table/ERC1363.md flat/ERC1363.flat.sol
 
-npx sol2uml -hn dist/ERC1363.dist.sol -o analysis/uml/ERC1363.svg
+npx sol2uml -hn flat/ERC1363.flat.sol -o analysis/uml/ERC1363.svg
